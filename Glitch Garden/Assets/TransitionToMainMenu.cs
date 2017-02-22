30fa8cc0467676d0a7239c1b27a7f4e93f00f65c
Andrew Schmidthuber <aschmidthuber@gmail.com>
2017-02-21 21:30:11 -0600

    Main menu system, level loader, splash screen

---
+++ Glitch Garden/Assets/TransitionToMainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionToMainMenu : MonoBehaviour
+{
+    LevelManager levelManager;
+    // Use this for initialization
+    void Start()
+    {
+        levelManager = GameObject.FindObjectOfType<LevelManager>();
+        Invoke("LoadMainMenu", 3f);
+    }
+
+    // Update is called once per frame
+    void LoadMainMenu()
+    {
+
+        levelManager.LoadLevelWrapper("Main Menu");
+    }
+
+}
