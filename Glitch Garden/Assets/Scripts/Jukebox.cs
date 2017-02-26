ed7b39f2b79e930d3a6479c6515bb540b7095209
Andrew Schmidthuber <aschmidthuber@gmail.com>
2017-02-26 12:11:19 -0600

    added base scenes and loading in between them, broke loadnext scene

---
+++ Glitch Garden/Assets/Scripts/Jukebox.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Jukebox : MonoBehaviour
+{
+
+    public AudioClip[] bgMusicArray;
+    AudioSource audioSource;
+
+    void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        DontDestroyOnLoad(gameObject);
+
+    }
+
+
+
+
+    void OnLevelWasLoaded(int level)
+    {
+        PlayMusicTrack(level);
+    }
+
+    ///<summary> Plays the current music track and loops it </summary>
+
+
+    public void PlayMusicTrack(int levelIndex)
+    {
+        AudioClip clip = bgMusicArray[levelIndex];
+
+        if (clip)
+        {
+            audioSource.clip = clip;
+            audioSource.Play();
+            audioSource.loop = true;
+        }
+        else { Debug.Log("No music clip for index"); }
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
---
+++ Glitch Garden/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    static string lastPlayedLevel;
+
+    public static string[] playableLevels = { "Level_01", "Level_02", "Level_03", "Level_04" };
+
+    public static void LoadLevel(string name)
+    {
+        if (playableLevels.Contains(name))
+        {
+
+            lastPlayedLevel = name;
+
+        }
+
+        Debug.Log("Loading level " + name);
+
+        SceneManager.LoadScene(name);
+    }
+
+    public void LoadLevelWrapper(string name)
+    {
+        LoadLevel(name);
+
+
+    }
+
+    public void LoadCurrentWrapper()
+    {
+        ReloadCurrent();
+    }
+    public void LoadNextWrapper() { LoadNextLevel(); }
+    public void QuitGame()
+    {
+        Debug.Log("quit called");
+
+        Application.Quit();
+
+    }
+
+    /// <summary>
+    /// Loads the next level.
+    /// </summary>
+    public static void LoadNextLevel()
+    {
+        int levelIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        print("Levelindex is " + levelIndex);
+        Scene levelName = SceneManager.GetSceneAt(levelIndex);
+        print("levelname is" + levelName.name);
+        //LoadLevel(levelName);
+    }
+
+    public static void ReloadCurrent()
+    {
+        LoadLevel(lastPlayedLevel);
+    }
+}
+
