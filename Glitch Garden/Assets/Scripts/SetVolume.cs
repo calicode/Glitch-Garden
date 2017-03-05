f90a3f80dbab9af071b6e8bfcc28167ae10e0203
Andrew Schmidthuber <aschmidthuber@gmail.com>
2017-03-05 12:07:43 -0600

    Animations for attackers and defenders

---
+++ Glitch Garden/Assets/Scripts/SetVolume.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetVolume : MonoBehaviour
+
+{
+    public Jukebox jukeBox;
+    // Use this for initialization
+    void Start()
+    {
+        jukeBox = FindObjectOfType<Jukebox>();
+        jukeBox.SetVolume(PlayerPrefs_Manager.GetMasterVolume());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
