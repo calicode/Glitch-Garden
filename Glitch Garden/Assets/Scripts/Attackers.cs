9b1dd507ff96403797e7aa8e2d358d18ac5b935c
Andrew Schmidthuber <aschmidthuber@gmail.com>
2017-03-07 17:43:04 -0600

    added trigger colliders for all objects, added animation events to control movespeed

---
+++ Glitch Garden/Assets/Scripts/Attackers.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attackers : MonoBehaviour
+{
+    float walkSpeed;
+    bool isAttacking = true;
+
+    // Use this for initialization
+    void Start()
+
+    {
+        Rigidbody2D myRigidBody = gameObject.AddComponent<Rigidbody2D>();
+
+        myRigidBody.isKinematic = true;
+
+    }
+
+    /// <summary>
+    /// Sent when another object enters a trigger collider attached to this
+    /// object (2D physics only).
+    /// </summary>
+    /// <param name="other">The other Collider2D involved in this collision.</param>
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(gameObject.name + " collided with " + other.gameObject.name);
+    }
+
+    public void SetSpeed(float speed)
+    {
+        walkSpeed = speed;
+    }
+    // setup a method called below which will choose the right damage based on object name
+    // i don't like digging through animation events to find damage numbers
+
+
+    void StrikeCurrentTarget(float damage)
+    {
+        /*		switch (name){
+                    case 
+                }
+        */
+        Debug.Log(name + " did " + damage + " damage");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.left * walkSpeed * Time.deltaTime);
+    }
+}
---
+++ Glitch Garden/Assets/Scripts/Projectiles.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectiles : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Rigidbody2D myRigidBody = gameObject.AddComponent<Rigidbody2D>();
+        myRigidBody.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log(gameObject.name + " collided with " + other.gameObject.name);
+    }
+
+}
